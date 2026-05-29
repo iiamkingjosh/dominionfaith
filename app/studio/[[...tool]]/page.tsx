4dcf8a86357f46d224323a65c7004602d52e6edb
iiamkingjosh <joshua4moses@gmail.com>
2026-05-29 12:08:44 +0100

    feat: add sanity studio at /studio route

---
+++ app/studio/[[...tool]]/page.tsx
@@ -0,0 +1,11 @@
+// app/studio/[[...tool]]/page.tsx
+'use client'
+
+import { NextStudio } from 'next-sanity/studio'
+import config from '@/sanity.config'
+
+export const dynamic = 'force-dynamic'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
