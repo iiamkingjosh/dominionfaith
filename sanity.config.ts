ba7ae714aa6830762f1cc4025325ae426a4221df
iiamkingjosh <joshua4moses@gmail.com>
2026-05-29 17:33:15 +0100

    feat: add sanity vision plugin for GROQ query explorer in studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 // sanity.config.ts
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
 import { schemas } from './sanity/schemas'
 
 export default defineConfig({
@@ -9,6 +10,6 @@ export default defineConfig({
   projectId: '6lfjc9b1',
   dataset: 'production',
   basePath: '/studio',
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
   schema: { types: schemas },
 })
