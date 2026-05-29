d601f891510a5369d304081b08d56d80d59fb4a8
iiamkingjosh <joshua4moses@gmail.com>
2026-05-29 12:01:12 +0100

    feat: add sanity image url builder

---
+++ sanity/lib/image.ts
@@ -0,0 +1,10 @@
+// sanity/lib/image.ts
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { client } from './client'
+
+const builder = imageUrlBuilder(client)
+
+export function imageUrlFor(source: SanityImageSource) {
+  return builder.image(source)
+}
